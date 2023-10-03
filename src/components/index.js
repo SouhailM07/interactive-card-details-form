05c7e0f0b018d0943f5bc498b9e4ed5a2e731716
shadow <lightshadow416@gmail.com>
2023-10-03 02:05:42 +0100

    [BackCard] component was created

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Container } from "./Container/Container";
 export { default as FrontCard } from "./FrontCard/FrontCard";
+export { default as BackCard } from "./BackCard/BackCard";
