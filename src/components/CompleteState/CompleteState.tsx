b943e0c5d987528b66e4bf727a3ead2c14725f67
shadow <lightshadow416@gmail.com>
2023-11-20 20:38:48 +0100

    [CompleteState] component is now / md RESPOSIVE /

--- src/components/CompleteState/CompleteState.tsx
+++ src/components/CompleteState/CompleteState.tsx
@@ -8,11 +8,9 @@ export default function CompleteState() {
         <img src={completeLogo} alt="img" />
         <div id="CompleteState-middle">
           <h1>THANK YOU!</h1>
-          <p className="">We've added your card details</p>
+          <p>We've added your card details</p>
         </div>
-        <button className="bg-violet-950  text-white w-full lg:py-[1rem] md:py-[1vw] rounded-md text-[1.3rem] mbl:py-[1rem]">
-          Continue
-        </button>
+        <button>Continue</button>
       </div>
     </>
   );
